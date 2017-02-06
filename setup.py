0b0e93bf595c8b1ebd0b93dba3dec90e0fd4b57b
Grahame Bowland <grahame@angrygoats.net>
2017-02-06 15:41:10 +0800

    generated datatype for safeguard import

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-chevrontheme',
-    version='0.0.2',
+    version='0.1.0',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
