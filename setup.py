6279f4a5eddd9651fb6a21ecfd3ef2843023e9b7
Grahame Bowland <grahame@angrygoats.net>
2017-03-24 11:49:40 +0800

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-chevrontheme',
-    version='0.4.1',
+    version='0.5.0',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
