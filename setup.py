faab70a450a555bd97953f1ecd7b3c1462f9a99c
Grahame Bowland <grahame@angrygoats.net>
2017-05-17 16:27:09 +0800

    PaDIL image upload support

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-chevrontheme',
-    version='0.7.1',
+    version='0.8.0',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
