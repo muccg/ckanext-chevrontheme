3913bcd07f371f58b1fe01ec18024f33227c237f
Grahame Bowland <grahame@angrygoats.net>
2017-08-31 14:19:52 +0800

    standardise specimen_type field

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-chevrontheme',
-    version='0.13.0',
+    version='0.13.1',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
