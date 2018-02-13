65f36c96701958a602d8185b495415519f22f55c
Sam Chang <schang@ccg.murdoch.edu.au>
2018-02-13 15:20:03 +0800

    bump version to 0.15.11

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-chevrontheme',
-    version='0.15.10',
+    version='0.15.11',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
